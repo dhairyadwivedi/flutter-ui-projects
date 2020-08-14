bdcdb5106503a094df2a858d38fd33c572a3e2a3
dhairyadwivedi <dhairiya96@gmail.com>
2020-08-14 22:11:54 +0530

    add rooms widget

--- facebook_ui/lib/screens/home_screen.dart
+++ facebook_ui/lib/screens/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_facebook_responsive_ui/config/palette.dart';
-import 'package:flutter_facebook_responsive_ui/widgets/circle_button.dart';
+import 'package:flutter_facebook_responsive_ui/data/data.dart';
+import 'package:flutter_facebook_responsive_ui/widgets/rooms.dart';
+import 'package:flutter_facebook_responsive_ui/widgets/widgets.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -35,6 +37,14 @@ class HomeScreen extends StatelessWidget {
                 onPressed: () => print('Messenger'),
               ),
             ],
+          ),
+          SliverToBoxAdapter(
+            child: CreatePostContainer(currentUser: currentUser),
+          ),
+          SliverPadding(
+            padding: const EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 5.0),
+            sliver: SliverToBoxAdapter(child: Rooms(onlineUsers: onlineUsers),
+            ),
           ),
         ],
       ),
