949f184b98a40b3975edbabe9cae7cd998e31954
dhairyadwivedi <dhairiya96@gmail.com>
2020-08-18 01:31:41 +0530

    add contact list and more options list

--- facebook_ui/lib/widgets/rooms.dart
+++ facebook_ui/lib/widgets/rooms.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_facebook_responsive_ui/config/palette.dart';
 import 'package:flutter_facebook_responsive_ui/models/models.dart';
 import 'package:flutter_facebook_responsive_ui/widgets/profile_avatar.dart';
+import 'package:flutter_facebook_responsive_ui/widgets/widgets.dart';
 
 class Rooms extends StatelessWidget {
   final List<User> onlineUsers;
@@ -11,28 +12,36 @@ class Rooms extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 60.0,
-      child: ListView.builder(
-        padding: const EdgeInsets.symmetric(horizontal: 4.0, vertical: 10.0),
-        scrollDirection: Axis.horizontal,
-        itemCount: 1 + onlineUsers.length,
-        itemBuilder: (BuildContext context, int index) {
-          if (index == 0) {
+    final bool isDesktop = Responsive.isDesktop(context);
+    return Card(
+      margin: EdgeInsets.symmetric(horizontal: isDesktop ? 5.0 : 0.0),
+      elevation: isDesktop ? 1.0 : 0.0,
+      shape: isDesktop
+          ? RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0))
+          : null,
+      child: Container(
+        height: 60.0,
+        child: ListView.builder(
+          padding: const EdgeInsets.symmetric(horizontal: 4.0, vertical: 10.0),
+          scrollDirection: Axis.horizontal,
+          itemCount: 1 + onlineUsers.length,
+          itemBuilder: (BuildContext context, int index) {
+            if (index == 0) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: _CreateRoomButton(),
+              );
+            }
+            final User user = onlineUsers[index - 1];
             return Padding(
               padding: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: _CreateRoomButton(),
+              child: ProfileAvatar(
+                imageUrl: user.imageUrl,
+                isActive: true,
+              ),
             );
-          }
-          final User user = onlineUsers[index - 1];
-          return Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: ProfileAvatar(
-              imageUrl: user.imageUrl,
-              isActive: true,
-            ),
-          );
-        },
+          },
+        ),
       ),
     );
   }
@@ -68,7 +77,9 @@ class _CreateRoomButton extends StatelessWidget {
             size: 35.0,
             color: Colors.purple,
           ),
-          const SizedBox(width: 8.0,),
+          const SizedBox(
+            width: 8.0,
+          ),
           Text('Create\nRoom'),
         ],
       ),
--- facebook_ui/lib/widgets/stories.dart
+++ facebook_ui/lib/widgets/stories.dart
@@ -15,7 +15,7 @@ class Stories extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 200.0,
-      color: Colors.white,
+      color: Responsive.isDesktop(context) ? Colors.transparent : Colors.white,
       child: ListView.builder(
         padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 10.0),
         scrollDirection: Axis.horizontal,
@@ -69,6 +69,15 @@ class _StoryCard extends StatelessWidget {
           decoration: BoxDecoration(
             gradient: Palette.storyGradient,
             borderRadius: BorderRadius.circular(12.0),
+            boxShadow: Responsive.isDesktop(context)
+                ? const [
+                    BoxShadow(
+                      color: Colors.black26,
+                      offset: Offset(0, 2),
+                      blurRadius: 4.0,
+                    )
+                  ]
+                : null,
           ),
         ),
         Positioned(
